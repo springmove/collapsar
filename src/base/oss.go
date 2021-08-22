8fcee80f4d76e6ef2728314d76024f875f226e3b
linshenqi <linshenqi@outlook.com>
2021-08-22 18:08:20 +0800

    1. update ListObjects

--- src/base/oss.go
+++ src/base/oss.go
@@ -15,7 +15,7 @@ const (
 type IOssService interface {
 	Upload(endpoint string, key string, data []byte) error
 	Delete(endpoint string, key string) error
-	ListObjects(endpoint string, token string) ([]string, string, error)
+	ListObjects(endpoint string, prefix string, token string) ([]string, string, error)
 }
 
 type Endpoint struct {
